6a97e5d52e6fa78188be1b14c2468615aa8ffedb
William Billingsley <wbillingsley@cantab.net>
2020-07-22 20:27:12 +1000

    Added skeleton code for ObjectInspector to make compile error go away
    This is a very temporary commit. ObjectInspector is barely even there.

---
+++ src/main/scala/structures/ObjectInspector.scala
@@ -0,0 +1,44 @@
+package structures
+
+import coderunner.Codable
+import coderunner.Codable.Triple
+import com.wbillingsley.veautiful.DiffNode
+import com.wbillingsley.veautiful.html.{<, VHtmlComponent, VHtmlNode}
+import org.scalajs.dom.{Element, Node}
+
+import scala.collection.mutable
+import scala.scalajs.js
+
+/**
+ * Incomplete. ObjectInspector is intended (in future releases) to be a Codable that can visualise the state of a
+ * JavaScript object.
+ */
+case class ObjectInspector() extends Codable {
+
+  private val underInspection = mutable.Buffer.empty[js.Dynamic]
+
+  private object renderComponent extends VHtmlComponent {
+
+    override protected def render: DiffNode[Element, Node] = <.div(
+    )
+  }
+
+  def addItem(item:js.Dynamic): Unit = {
+    println("The type is " + item.getTypeOf)
+  }
+
+  override def reset(): Unit = {
+
+  }
+
+  override def start(): Unit = {
+
+  }
+
+  override def functions(): Seq[(String, Seq[String], js.Function)] = Seq[Triple](
+    ("refresh", Seq.empty, () => { renderComponent.rerender() }),
+    ("addItem", Seq("Any"), (x:js.Dynamic) => addItem(x) )
+  )
+
+  override def vnode: VHtmlNode = renderComponent
+}
